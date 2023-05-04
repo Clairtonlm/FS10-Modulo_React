2b966e33a88fc651fc8d4ca83cb059b18b521b82
Clairtonlm <clairtonlm@gmail.com>
2023-05-03 21:55:13 -0300

    Condicional

--- Vite-react/vite-project/src/App.jsx
+++ Vite-react/vite-project/src/App.jsx
@@ -9,41 +9,37 @@ const noticias = [
   {
     titulo: "titulo 1",
     descricao: "descrição 1",
-    link: "link 1"
+    link: "link 1",
+    status: true,
   },
   {
     titulo: "titulo 2",
     descricao: "descrição 2",
-    link: "link 2"
+    link: "link 2",
+    status: false,
   },
   {
     titulo: "titulo 3",
     descricao: "descrição 3",
-    link: "link 3"
+    link: "link 3",
+    status: true,
   },
 ];
 
   return (
     <>
+
     <section id="cartoes">
-      <Card 
-      titulo1="Meu titulo"
-      descricao1="descrição 1"
-      link1="#!"
-      />
-      <Card 
-      titulo2="titulo 2"
-      descricao2="descrição 2"
-      link2="#"
-      />
-      <Card 
-      titulo3="titulo 3"
-      descricao3="descrição 3"
-      link3="#"
-      />
-      <Card />
-      <Card />
-    </section>
+      {
+        noticias.map((noticia)=>{
+          return noticia.status &&  <Card 
+                      titulo={noticia.titulo} 
+                      descricao={noticia.descricao}
+                      link={noticia.link} />
+                      
+        })
+      }
+   
       
     </>
   );
