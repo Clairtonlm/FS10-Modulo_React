a68320c34055a2b730eccd4e9501a13af61f75d2
Clairtonlm <clairtonlm@gmail.com>
2023-05-03 21:23:14 -0300

    Criando cards

---
+++ Vite-react/vite-project/src/componentes/cards/index.jsx
@@ -0,0 +1,14 @@
+const Card = ( ) => {
+    return (
+        <>
+        <div>
+            
+            <h3>Titulo do cartão</h3>
+            <p>Lorem ipsum dolor  odio rem reiciendis voluptates quas. Quidem quia veniam quisquam ab impedit architecto tempore iure cum et culpa tenetur, illum veritatis!</p>
+            <a href="#">Link</a>
+        </div>
+
+        </>
+    )
+}
+export default Card;
