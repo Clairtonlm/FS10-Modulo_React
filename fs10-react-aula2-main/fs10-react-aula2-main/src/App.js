de075f1e38282782342494aaf38e60931cc4e5f0
Clairtonlm <clairtonlm@gmail.com>
2023-05-10 20:43:58 -0300

    Página NotFound

--- fs10-react-aula2-main/fs10-react-aula2-main/src/App.js
+++ fs10-react-aula2-main/fs10-react-aula2-main/src/App.js
@@ -2,8 +2,10 @@
 import './App.css';
 import Contato from './components/Contatos';
 //Componentes
-import Dashboard from './components/Dashboard';
-import Header from './components/Header';
+
+import PageLayout from './components/Layouts/PageLayout';
+import NotFound from './components/NotFound';
+
 import Portifolio from './components/Portifolio';
 import Sobre from './components/Sobre';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -11,20 +13,21 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 const App = () => {
   return(
       <>
-        <Header />
-        <main>
+        
+        
           <BrowserRouter>
           <Routes>
-            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/' element={<PageLayout/>}/>
             <Route path='/sobre' element={<Sobre/>}/>
             <Route path='/Potifolio' element={<Portifolio/>}/>
             <Route path='/Contato' element={<Contato/>}/>
+            <Route path='*' element={<NotFound/>}/>
             </Routes>  
 
           </BrowserRouter>
           
           
-        </main>
+        
         
       </>
   );
