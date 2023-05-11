dba9627c8982f91880cd445548d7938a47efa054
Clairtonlm <clairtonlm@gmail.com>
2023-05-10 21:17:44 -0300

    UseState para incrementar

--- fs10-react-aula2-main/fs10-react-aula2-main/src/components/Dashboard/index.jsx
+++ fs10-react-aula2-main/fs10-react-aula2-main/src/components/Dashboard/index.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashboard = () => {
+    //aqui vai javaScript
+    const [contador, setContador] = useState(0)
     return(
         <>
             <h1>Dashboard</h1>
-            
+            <button onClick={()=> setContador(contador +1)}>incrementar{contador}</button>
         </>
     );
 }
